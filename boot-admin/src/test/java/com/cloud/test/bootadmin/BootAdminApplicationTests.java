11a80b638de04052e8ce6f71ce6563253234c882
宋召 <herogmaii@gmail.com>
2019-12-25 14:18:19 +0800

    add admin

---
+++ boot-admin/src/test/java/com/cloud/test/bootadmin/BootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.test.bootadmin;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminApplicationTests {
+    
+    @Test
+    void contextLoads(){
+    }
+}
