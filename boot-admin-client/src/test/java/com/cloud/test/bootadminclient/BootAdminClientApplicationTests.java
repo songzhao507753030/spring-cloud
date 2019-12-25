ee4a44650759a406dc124787f1e66efddaa68907
宋召 <herogmaii@gmail.com>
2019-12-25 14:56:27 +0800

    update admin

---
+++ boot-admin-client/src/test/java/com/cloud/test/bootadminclient/BootAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.test.bootadminclient;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminClientApplicationTests {
+    
+    @Test
+    void contextLoads(){
+    }
+}
---
+++ turbine-master/src/test/java/com/boot/test/bootadmin/BootAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.test.bootadmin;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootAdminApplicationTests {
+
+    @Test
+    void contextLoads(){
+    }
+}
