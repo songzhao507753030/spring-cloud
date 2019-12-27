892e62965719894e4fa591cbbca215af2fbc7632
宋召 <herogmaii@gmail.com>
2019-12-27 13:29:25 +0800

    短信通知

--- boot-admin-client/src/main/java/com/cloud/test/bootadminclient/BootAdminClientApplication.java
+++ boot-admin-client/src/main/java/com/cloud/test/bootadminclient/BootAdminClientApplication.java
@@ -3,10 +3,11 @@ package com.cloud.test.bootadminclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BootAdminClientApplication {
-    
     public static void main(String[] args){
         SpringApplication.run(BootAdminClientApplication.class,args);
     }
--- boot-admin-server/src/main/java/com/cloud/test/bootadmin/BootAdminServerApplication.java
+++ boot-admin-server/src/main/java/com/cloud/test/bootadmin/BootAdminServerApplication.java
@@ -4,8 +4,10 @@ package com.cloud.test.bootadmin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableAdminServer
+@EnableDiscoveryClient   //开启一个eureka客户端
+@EnableAdminServer   //开启一个监控服务
 @SpringBootApplication
 public class BootAdminServerApplication {
     public static void main(String[] args){
